897b24a01bebd412fef55fe204f15115737865be
snappyjel <leviftr1@gmail.com>
2023-05-29 20:43:55 +0700

    Creating a function for player input

--- javascript.js
+++ javascript.js
@@ -2,22 +2,27 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   switch (choice) {
     case 0:
-      return "Rock";
+      return "rock";
       break;
     case 1:
-      return "Paper";
+      return "paper";
       break;
     default:
-      return "Scissors";
+      return "scissors";
       break;
   }
 }
+
+function getPlayerChoice() {
+  let player = prompt("What will you play?");
+  return player.toLowerCase();
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     alert("It's a tie! Good luck next round");
   }
 }
-
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
