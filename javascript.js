d43bacffda828a116359f057d21a41b8bdbe9b18
snappyjel <leviftr1@gmail.com>
2023-05-31 13:45:12 +0700

    Page styling

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function getComputerChoice() {
     case 1:
       return "paper";
       break;
-    default:
+    case 2:
       return "scissors";
       break;
   }
@@ -18,30 +18,30 @@ const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
 
-function getPlayerChoice() {
-  let playerInput = prompt("What will you play?");
-  let getPlayerChoice = playerInput.toLowerCase();
-  console.log(getPlayerChoice);
-  return getPlayerChoice;
-}
+// function getPlayerChoice() {
+//   let playerInput = prompt("What will you play?");
+//   let getPlayerChoice = playerInput.toLowerCase();
+//   console.log(getPlayerChoice);
+//   return getPlayerChoice;
+// }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    alert("It's a tie! Good luck next round");
+    return "It's a tie! Good luck next round";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "sciccors" && computerSelection === "paper")
   ) {
-    alert("You win!");
+    return "You win!";
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
-    alert("You lose!");
+    return "You lose!";
   } else {
-    alert("I break!");
+    return "I break!";
   }
 }
 // TODO: Fix the playRound function
